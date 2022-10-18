7fa94e17514604044a33e0675fb2d37a5dd8d7da
Nitin <nk17oct@gmail.com>
2022-10-18 23:01:47 +0530

    add controllers

---
+++ controllers/campgrounds.js
@@ -0,0 +1,53 @@
+const Campground = require('../models/campground');
+
+module.exports.index = async (req,res)=>{
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', {campgrounds});
+}
+
+module.exports.createCampground = async (req,res)=>{
+    const campground = new Campground(req.body.campground);
+    campground.author = req.user.id;
+    await campground.save();
+    req.flash('success', 'Successfully created campground');
+    res.redirect(`/campgrounds/${campground._id}`); 
+}
+
+module.exports.renderNewForm = (req,res)=>{
+    res.render('campgrounds/new');
+}
+
+module.exports.showCampground = async (req,res)=>{
+    const campground = await Campground.findById(req.params.id).populate({
+        path: 'reviews',
+        populate: {
+            path: 'author'
+        }
+    }).populate('author');
+    if(!campground){
+        req.flash('error', 'Campground not found');
+        res.redirect('/campgrounds');
+    }
+    res.render('campgrounds/show', { campground });
+}
+
+module.exports.updateCampground = async(req,res)=>{
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    req.flash('success', 'Campground edited successfully');
+    res.redirect(`/campgrounds/${campground._id}`);
+}
+
+module.exports.deleteCampground = async(req,res)=>{
+    await Campground.findByIdAndDelete(req.params.id);
+    req.flash('success', 'Campground deleted successfully');
+    res.redirect('/campgrounds');
+}
+
+module.exports.renderEditForm = async (req,res)=>{
+    const campground = await Campground.findById(req.params.id);
+    if(!campground){
+        req.flash('error', 'Campground not found');
+        res.redirect('/campgrounds');
+    }
+    res.render('campgrounds/edit', { campground });
+}
--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
-const User = require("../models/user");
+const users = require("../controllers/users");
 
-router.get("/register", (req, res) => {
-  res.render("users/register");
-});
+router.get("/register", users.renderRegister);
 
-router.post("/register", async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
-    const user = new User({ email, username });
-    const registeredUser = await User.register(user, password);
-    req.login(registeredUser, err => {
-      if(err) return next(err);
-      req.flash("success", "Welcome to Yelp Camp!");
-      res.redirect("/campgrounds");
-    });
-  } catch (err) {
-    req.flash("error", err.message);
-    res.redirect("/register");
-  }
-});
+router.post("/register", users.register);
 
-router.get("/login", (req, res) => {
-  res.render("users/login");
-});
+router.get("/login", users.renderLogin);
 
 router.post(
   "/login",
@@ -33,20 +15,9 @@ router.post(
     failureFlash: true,
     failureRedirect: "/login",
   }),
-  (req, res) => {
-    req.flash("success", "Welcome back!");
-    // TODO: Return url doesn't work 
-    // passport overwrites the session
-    const redirectUrl = req.session.returnTo || '/campgrounds';
-    delete req.session.returnTo;
-    res.redirect(redirectUrl);
-  }
+  users.login
 );
 
-router.get("/logout", (req, res) => {
-  req.logout(()=>{});
-  req.flash("success", "Successfully logged out!");
-  res.redirect("/campgrounds");
-});
+router.get("/logout", users.logout);
 
 module.exports = router;
