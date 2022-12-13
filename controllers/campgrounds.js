02a5f1bc7f61d196302cb44c8b6f158f86e9bb58
Nitin <nk17oct@gmail.com>
2022-12-13 21:09:47 +0530

    cloudinary image upload

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -7,6 +7,7 @@ module.exports.index = async (req,res)=>{
 
 module.exports.createCampground = async (req,res)=>{
     const campground = new Campground(req.body.campground);
+    campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.author = req.user.id;
     await campground.save();
     req.flash('success', 'Successfully created campground');
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
-const campgrounds = require('../controllers/campgrounds');
+const multer = require('multer');
+const { storage } = require('../cloudinary');
+const upload = multer({ storage })
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground));
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
